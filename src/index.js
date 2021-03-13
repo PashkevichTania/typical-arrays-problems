2b73362fac61fcb5c57482fc3f6811296e0d2643
PashkevichTania <pashkevichtania2004@gmail.com>
2021-03-13 18:04:59 +0300

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (typeof array !== 'undefined' && array.length > 0) {
+        let min = array[0];
+        for (let i of array) {
+            if (i < min) {
+                min = i;
+            }
+        }
+        return min;
+    }
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (typeof array !== 'undefined' && array.length > 0) {
+        let sum = 0;
+        for (let i of array) {
+            sum += i;
+        }
+        return (sum / array.length).toFixed(2);
+    }
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof array !== 'undefined' && array.length > 0) {
+        let max = 0;
+        for (let i of array) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        return max;
+    }
+    return 0;
 }
